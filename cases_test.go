0928b7f6f5e078b9a5c12d3eb423159dd174ac51
pomntv <pomntv@gmail.com>
2023-05-23 01:38:43 +0700

    update coding stlye

--- cases_test.go
+++ cases_test.go
@@ -1,6 +1,5 @@
 package phonenumber
 
-
 var numberTests = []struct {
 	description      string
 	input            string
@@ -9,6 +8,7 @@ var numberTests = []struct {
 	number           string
 	areaCode         string
 	formatted        string
+	index            int
 }{
 	{
 		description: "cleans the number",
@@ -16,6 +16,7 @@ var numberTests = []struct {
 		number:      "2234567890",
 		areaCode:    "223",
 		formatted:   "(223) 456-7890",
+		index:       1,
 	},
 	{
 		description: "cleans numbers with dots",
@@ -23,6 +24,7 @@ var numberTests = []struct {
 		number:      "2234567890",
 		areaCode:    "223",
 		formatted:   "(223) 456-7890",
+		index:       2,
 	},
 	{
 		description: "cleans numbers with multiple spaces",
@@ -30,18 +32,21 @@ var numberTests = []struct {
 		number:      "2234567890",
 		areaCode:    "223",
 		formatted:   "(223) 456-7890",
+		index:       3,
 	},
 	{
 		description:      "invalid when 9 digits",
 		input:            "123456789",
 		expectErr:        true,
 		errorDescription: "incorrect number of digits",
+		index:            4,
 	},
 	{
 		description:      "invalid when 11 digits does not start with a 1",
 		input:            "22234567890",
 		expectErr:        true,
 		errorDescription: "11 digits must start with 1",
+		index:            5,
 	},
 	{
 		description: "valid when 11 digits and starting with 1",
@@ -49,6 +54,7 @@ var numberTests = []struct {
 		number:      "2234567890",
 		areaCode:    "223",
 		formatted:   "(223) 456-7890",
+		index:       6,
 	},
 	{
 		description: "valid when 11 digits and starting with 1 even with punctuation",
@@ -56,71 +62,83 @@ var numberTests = []struct {
 		number:      "2234567890",
 		areaCode:    "223",
 		formatted:   "(223) 456-7890",
+		index:       7,
 	},
 	{
 		description:      "invalid when more than 11 digits",
 		input:            "321234567890",
 		expectErr:        true,
 		errorDescription: "more than 11 digits",
+		index:            8,
 	},
 	{
 		description:      "invalid with letters",
 		input:            "123-abc-7890",
 		expectErr:        true,
 		errorDescription: "letters not permitted",
+		index:            9,
 	},
 	{
 		description:      "invalid with punctuations",
 		input:            "123-@:!-7890",
 		expectErr:        true,
 		errorDescription: "punctuations not permitted",
+		index:            10,
 	},
 	{
 		description:      "invalid if area code starts with 0",
 		input:            "(023) 456-7890",
 		expectErr:        true,
 		errorDescription: "area code cannot start with zero",
+		index:            11,
 	},
 	{
 		description:      "invalid if area code starts with 1",
 		input:            "(123) 456-7890",
 		expectErr:        true,
 		errorDescription: "area code cannot start with one",
+		index:            12,
 	},
 	{
 		description:      "invalid if exchange code starts with 0",
 		input:            "(223) 056-7890",
 		expectErr:        true,
 		errorDescription: "exchange code cannot start with zero",
+		index:            13,
 	},
 	{
 		description:      "invalid if exchange code starts with 1",
 		input:            "(223) 156-7890",
 		expectErr:        true,
 		errorDescription: "exchange code cannot start with one",
+		index:            14,
 	},
 	{
 		description:      "invalid if area code starts with 0 on valid 11-digit number",
 		input:            "1 (023) 456-7890",
 		expectErr:        true,
 		errorDescription: "area code cannot start with zero",
+		index:            15,
 	},
 	{
 		description:      "invalid if area code starts with 1 on valid 11-digit number",
 		input:            "1 (123) 456-7890",
 		expectErr:        true,
 		errorDescription: "area code cannot start with one",
+		index:            16,
 	},
 	{
 		description:      "invalid if exchange code starts with 0 on valid 11-digit number",
 		input:            "1 (223) 056-7890",
 		expectErr:        true,
 		errorDescription: "exchange code cannot start with zero",
+		index:            17,
 	},
 	{
 		description:      "invalid if exchange code starts with 1 on valid 11-digit number",
 		input:            "1 (223) 156-7890",
 		expectErr:        true,
 		errorDescription: "exchange code cannot start with one",
+		index:            18,
 	},
 }
--- phone_number_test.go
+++ phone_number_test.go
@@ -1,24 +1,35 @@
 package phonenumber
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestNumber(t *testing.T) {
 	for _, test := range numberTests {
 		actual, actualErr := Number(test.input)
+		fmt.Printf("###> TestNumber ### ")
+		if actual != "" {
+			fmt.Printf("%v: Number: %v\n", test.index, actual)
+		} else {
+			fmt.Printf("%v: !!! actualErr: %v\n", test.index, actualErr)
+		}
 		if !test.expectErr {
 			if actualErr != nil {
 				var _ error = actualErr
 				t.Errorf("FAIL: %s\nNumber(%q): expected no error, but error is: %s", test.description, test.input, actualErr)
+				fmt.Printf("FAIL: %s\nNumber(%q): expected no error, but error is: %s \n", test.description, test.input, actualErr)
 			}
 			if actual != test.number {
 				t.Errorf("FAIL: %s\nNumber(%q): expected [%s], actual: [%s]", test.description, test.input, test.number, actual)
+				fmt.Printf("FAIL: %s\nNumber(%q): expected [%s], actual: [%s] \n", test.description, test.input, test.number, actual)
 			}
 		} else if actualErr == nil {
 			t.Errorf("FAIL: %s\nNumber(%q): expected an error, but error is nil", test.description, test.input)
+			fmt.Printf("FAIL: %s\nNumber(%q): expected an error, but error is nil \n", test.description, test.input)
 		}
 	}
+
 }
 
 func BenchmarkNumber(b *testing.B) {
@@ -32,16 +43,25 @@ func BenchmarkNumber(b *testing.B) {
 func TestAreaCode(t *testing.T) {
 	for _, test := range numberTests {
 		actual, actualErr := AreaCode(test.input)
+		fmt.Printf("####> TestAreaCode ### ")
+		if actual != "" {
+			fmt.Printf("%v: AreaCode: %v\n", test.index, actual)
+		} else {
+			fmt.Printf("%v: !!! actualErr: %v\n", test.index, actualErr)
+		}
 		if !test.expectErr {
 			if actual != test.areaCode {
 				t.Errorf("FAIL: %s\nAreaCode(%q): expected [%s], actual: [%s]", test.description, test.input, test.areaCode, actual)
+				fmt.Printf("FAIL: %s\nAreaCode(%q): expected [%s], actual: [%s] \n", test.description, test.input, test.areaCode, actual)
 			}
 			if actualErr != nil {
 				var _ error = actualErr
 				t.Errorf("FAIL: %s\nAreaCode(%q): expected no error, but error is: %s", test.description, test.input, actualErr)
+				fmt.Printf("FAIL: %s\nAreaCode(%q): expected no error, but error is: %s \n", test.description, test.input, actualErr)
 			}
 		} else if actualErr == nil {
 			t.Errorf("FAIL: %s\nAreaCode(%q): expected an error, but error is nil", test.description, test.input)
+			fmt.Printf("FAIL: %s\nAreaCode(%q): expected an error, but error is nil \n", test.description, test.input)
 		}
 	}
 }
@@ -57,16 +77,26 @@ func BenchmarkAreaCode(b *testing.B) {
 func TestFormat(t *testing.T) {
 	for _, test := range numberTests {
 		actual, actualErr := Format(test.input)
+		fmt.Printf("#####> TestFormat ### ")
+		if actual != "" {
+			fmt.Printf("%v: Format: %v\n", test.index, actual)
+		} else {
+			fmt.Printf("%v: !!! actualErr: %v\n", test.index, actualErr)
+		}
 		if !test.expectErr {
 			if actualErr != nil {
 				var _ error = actualErr
 				t.Errorf("FAIL: %s\nFormat(%q): expected no error, but error is: %s", test.description, test.input, actualErr)
+				fmt.Printf("FAIL: %s\nFormat(%q): expected no error, but error is: %s \n", test.description, test.input, actualErr)
 			}
 			if actual != test.formatted {
 				t.Errorf("FAIL: %s\nFormat(%q): expected [%s], actual: [%s]", test.description, test.input, test.formatted, actual)
+				fmt.Printf("FAIL: %s\nFormat(%q): expected [%s], actual: [%s] \n", test.description, test.input, test.formatted, actual)
 			}
 		} else if actualErr == nil {
 			t.Errorf("FAIL: %s\nFormat(%q): expected an error, but error is nil", test.description, test.input)
+			fmt.Printf("FAIL: %s\nFormat(%q): expected an error, but error is nil \n", test.description, test.input)
+
 		}
 	}
 }
